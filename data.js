a9ad6489b0e2b73545ce36f35a3a04e5b4bbdee5
Dalenza <dalydiiheb@gmail.com>
2022-07-03 19:25:11 +0200

    initialize repo

---
+++ data.js
@@ -0,0 +1,86 @@
+const cs1 = [
+    {
+        name: "Added Haythem",
+        subjects: [
+            {
+                title: "entrepot de données",
+                examen: 18,
+                ds: 14.25,
+                moy: 16.88,
+                credits: 2
+            },
+            {
+                title: "Administration des bases de données",
+                examen: 18.25,
+                ds: 19,
+                tp: 16,
+                moy: ,
+                credits: 2
+            },
+            {
+                title: "entrepot de données",
+                examen: 18,
+                ds: 14.25,
+                moy: 16.88,
+                credits: 2
+            },
+            {
+                title: "entrepot de données",
+                examen: 18,
+                ds: 14.25,
+                moy: 16.88,
+                credits: 2
+            },
+            {
+                title: "entrepot de données",
+                examen: 18,
+                ds: 14.25,
+                moy: 16.88,
+                credits: 2
+            },
+            {
+                title: "entrepot de données",
+                examen: 18,
+                ds: 14.25,
+                moy: 16.88,
+                credits: 2
+            },
+            {
+                title: "entrepot de données",
+                examen: 18,
+                ds: 14.25,
+                moy: 16.88,
+                credits: 2
+            },
+            {
+                title: "entrepot de données",
+                examen: 18,
+                ds: 14.25,
+                moy: 16.88,
+                credits: 2
+            },
+            {
+                title: "entrepot de données",
+                examen: 18,
+                ds: 14.25,
+                moy: 16.88,
+                credits: 2
+            },
+            {
+                title: "entrepot de données",
+                examen: 18,
+                ds: 14.25,
+                moy: 16.88,
+                credits: 2
+            }
+        ]
+    },
+    {
+        name: "Aljane Alaa",
+        subjects: [
+            {
+
+            }
+        ]
+    }
+]
