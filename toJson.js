e10f6bbc5093aa9ce4b1db41bed2715d05f0e8f6
Dalenza <dalydiiheb@gmail.com>
2022-07-04 16:33:29 +0200

    populate database

--- toJson.js
+++ toJson.js
@@ -5,7 +5,7 @@ const workBook = xlsx.readFile("./CCF25082020_0003.xlsx")
 
 function createJsonFile() {
     for ([sheetName, sheetValue] of Object.entries(workBook.Sheets)) {
-        if (sheetName.startsWith("moy")) {
+        if (!sheetName.startsWith("Table 1")) {
             let jsonData = xlsx.utils.sheet_to_json(sheetValue)
             fs.writeFile(`./database/${sheetName}.json`, JSON.stringify(jsonData), () => {
                 console.log("file written")
