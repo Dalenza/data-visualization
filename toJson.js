1a9941d279d92ed8914d8c26e8c8690801289bf7
Dalenza <dalydiiheb@gmail.com>
2022-07-05 09:38:40 +0200

    repopulate database

--- toJson.js
+++ toJson.js
@@ -3,7 +3,7 @@ const xlsx = require("xlsx")
 
 const workBook = xlsx.readFile("./CCF25082020_0003.xlsx")
 
-function createJsonFile() {
+function populateDatabase() {
     for ([sheetName, sheetValue] of Object.entries(workBook.Sheets)) {
         if (!sheetName.startsWith("Table 1")) {
             let jsonData = xlsx.utils.sheet_to_json(sheetValue)
@@ -14,4 +14,4 @@ function createJsonFile() {
     }
 }
 
-createJsonFile()
+populateDatabase()
