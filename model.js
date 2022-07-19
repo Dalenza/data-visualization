9d61ad0030bd50dbed3d27caaf0ebcb7156b82d8
amine-a11 <mohamedaminehajji11@yahoo.com>
2022-07-19 19:24:29 +0100

    adding the api

--- model.js
+++ model.js
@@ -23,21 +23,22 @@ function jsonRead(path) {
     return JSON.parse(data)
 }
 /**
- * 
+ * @param {string} group
  * @returns an array of student marks(objects)
  */
-function getStudentsMarks() {
-    return jsonRead("./database/L2CS01.json")
+function getStudentsMarks(group) {
+    return jsonRead(`./database/${group}.json`)
 }
 
 /**
  * gets the specified student's mark for a specified subject  or all marks
  * @param {string} studentName 
  * @param {string} subject 
+ * @param {string} group
  * @returns either an object containing all marks or an object containing the mark of the specified subject
  */
-function getStudentMark(studentName, subject = undefined) {
-    const studentMarks = jsonRead("./database/L2CS01.json")
+function getStudentMark(studentName, subject = undefined, group) {
+    const studentMarks = jsonRead(`./database/${group}.json`);
     studentName = studentName.toLowerCase();
 
     for (student of studentMarks) {
@@ -61,7 +62,7 @@ function getStudentMark(studentName, subject = undefined) {
  * @param {string} group e.g (L2CS01)
  * @returns {Number} returns a number representing the student's score
  */
-function getStudentMoy(studentName, semester = undefined, group) {
+function getStudentMoy(studentName, group, semester = undefined) {
     const studentScores = jsonRead(`./database/moy ${group.toUpperCase()}.json`)
     studentName = studentName.toLowerCase()
     for (student of studentScores) {
@@ -71,86 +72,9 @@ function getStudentMoy(studentName, semester = undefined, group) {
     }
 }
 
-/**
- * gets the student's rank in his class
- * @param {string} studentName
- * @param {string} group e.g (L2CS01)
- * @param {string} semester e.g (sem1|sem2)
- * @returns {Number} returns a number representing the student's rank in his class in the first semester or second semester or in general
- */
-function getStudentRankClass(studentName, group, semester = undefined) {
-    const studentScores = jsonRead(`./database/moy ${group.toUpperCase()}.json`)
-    let moy = [];
-    if (!semester) semester = "generale";
-    for (student of studentScores) {
-        moy.push([student[`moy ${semester}`], student.Name.toLowerCase()])
-    }
-    moy.sort((a, b) => {
-        return (a[0] >= b[0]) ? -1 : 1;
-    });
-    for (let i = 0; i < moy.length; i++) {
-        if (moy[i][1] === studentName.toLowerCase()) {
-            return i + 1;
-        }
-    }
+module.exports = {
+    getStudentMark,
+    getStudentsMarks,
+    getStudentMoy,
+    jsonRead,
 }
-/**
- * gets the student's rank in his "section"
- * @param {string} studentName
- * @param {string} semester e.g (sem1|sem2)
- * @returns {Number} returns a number representing the student's rank in his "section" in the first semester or second semester or in general
- */
-function getStudentRankSection(studentName, semester = undefined) {
-    if (!semester) semester = "generale";
-    let moy = [];
-    for (let i = 1; i <= 4; i++) {
-        const studentScores = jsonRead(`./database/moy L2CS0${i}.json`)
-        for (student of studentScores) {
-            moy.push([student[`moy ${semester}`], student.Name.toLowerCase()])
-        }
-    }
-    moy.sort((a, b) => {
-        return (a[0] >= b[0]) ? -1 : 1;
-    });
-    for (let i = 0; i < moy.length; i++) {
-        if (moy[i][1] === studentName.toLowerCase()) {
-            return i + 1;
-        }
-    }
-}
-/**
- * gets the student's rank in all subects;
- * @param {string} studentName
- * @returns {Number} returns the rank of each subject (object)
- */
-function getStudentRankSubjectsClass(studentName) {
-    const grades = getStudentsMarks();
-    let ans = {};
-    Object.keys(grades[0]).forEach(ele => {
-        ans[ele] = -1;
-    });
-
-    ans.Name = studentName;
-    let arr = Object.keys(ans).splice(1);
-    arr.forEach(ele => {
-        let moy = [];
-        for (grade of grades) {
-            moy.push([grade[ele] === 'disp' ? 0 : grade[ele], grade.Name]);
-        }
-        moy.sort((a, b) => {
-            return (a[0] >= b[0]) ? -1 : 1;
-        });
-        for (let i = 0; i < moy.length; i++) {
-            if (moy[i][1].toLowerCase() === studentName.toLowerCase()) {
-                ans[ele] = i + 1;
-                break;
-            }
-        }
-    });
-    return ans;
-
-}
-console.log(getStudentRankSubjectsClass("HAJJI MOHAMED AMINE"));
-//console.log(getStudentRankSubjectClass("HAJJI MOHAMED AMINE", "Ingénierie des bases de données"));
-//console.log(getStudentRankOverSection("HAJJI MOHAMED AMINE", "sem2"));
-//console.log(getStudentMoy("ZARROUKI MARIEM", "L2CS03", "sem2"));
