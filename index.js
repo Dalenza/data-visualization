a2ce1cd6f1ff57fa68d36363068933c68235e671
dalenza <dalydiiheb@gmail.com>
2022-07-22 11:17:58 +0100

    add client side app

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-const express = require('express')
-const homeRouter = require("./routers/home")
+const express = require("express");
+const homeRouter = require("./routers/home");
+const path = require("path");
 // const userRouter = require("./routes/user")
 
-const app = express()
-
+const app = express();
+const publicPath = path.resolve(__dirname, "views");
 // initialize routes
-app.use(homeRouter)
+app.use(homeRouter);
 // app.use("/users",userRouter)
-// listen for requests 
-app.listen(3000,()=>{
-    console.log('listening on port 3000')
-})
+// listen for requests
 
+app.use(express.static(publicPath));
+app.listen(3000, () => {
+  console.log("listening on port 3000");
+});
